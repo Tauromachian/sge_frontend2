e5a1b626ed30bb421e81d06309a0d97bf30b442a
Jose <jogarcia@tuta.io>
2020-01-18 14:13:55 -0500

    Minor toolbar fix

--- src/components/core/Toolbar.vue
+++ src/components/core/Toolbar.vue
@@ -1,21 +1,8 @@
 <template>
-  <v-app-bar
-    id="core-toolbar"
-    app
-    flat
-    style="background: #eee;"
-  >
+  <v-app-bar id="core-toolbar" flat style="background: #eee;">
     <div class="v-toolbar-title">
-      <v-toolbar-title
-        class="tertiary--text font-weight-light"
-      >
-        <v-btn
-          v-if="responsive"
-          class="default v-btn--simple"
-          dark
-          icon
-          @click.stop="onClickBtn"
-        >
+      <v-toolbar-title class="tertiary--text font-weight-light">
+        <v-btn v-if="responsive" class="default v-btn--simple" dark icon @click.stop="onClickBtn">
           <v-icon>mdi-view-list</v-icon>
         </v-btn>
         {{ title }}
@@ -24,65 +11,33 @@
 
     <v-spacer />
     <v-toolbar-items>
-      <v-flex
-        align-center
-        layout
-        py-2
-      >
-        <v-text-field
-          class="mr-4 purple-input"
-          label="Search..."
-          hide-details
-          color="purple"
-        />
-        <router-link
-          v-ripple
-          class="toolbar-items"
-          to="/"
-        >
+      <v-flex align-center layout py-2>
+        <v-text-field class="mr-4 purple-input" label="Search..." hide-details color="purple" />
+        <router-link v-ripple class="toolbar-items" to="/">
           <v-icon color="tertiary">mdi-view-dashboard</v-icon>
         </router-link>
-        <v-menu
-          bottom
-          left
-          content-class="dropdown-menu"
-          offset-y
-          transition="slide-y-transition">
-          <router-link
-            v-ripple
-            slot="activator"
-            class="toolbar-items"
-            to="/notifications"
-          >
-            <v-badge
-              color="error"
-              overlap
-            >
-              <template slot="badge">
-                {{ notifications.length }}
-              </template>
-              <v-icon color="tertiary">mdi-bell</v-icon>
-            </v-badge>
-          </router-link>
-          <v-card>
-            <v-list dense>
-              <v-list-item
-                v-for="notification in notifications"
-                :key="notification"
-                @click="onClick"
-              >
-                <v-list-title
-                  v-text="notification"
-                />
-              </v-list-item>
-            </v-list>
-          </v-card>
+        <v-menu bottom left content-class="dropdown-menu" offset-y transition="slide-y-transition">
+          <template v-slot:activator="{ on }">
+            <router-link v-ripple slot="activator" class="toolbar-items" to="/notifications">
+              <v-badge color="error" overlap>
+                <template slot="badge">{{ notifications.length }}</template>
+                <v-icon color="tertiary">mdi-bell</v-icon>
+              </v-badge>
+            </router-link>
+            <v-card>
+              <v-list dense>
+                <v-list-item
+                  v-for="notification in notifications"
+                  :key="notification"
+                  @click="onClick"
+                >
+                  <v-list-item-title v-text="notification" />
+                </v-list-item>
+              </v-list>
+            </v-card>
+          </template>
         </v-menu>
-        <router-link
-          v-ripple
-          class="toolbar-items"
-          to="/user-profile"
-        >
+        <router-link v-ripple class="toolbar-items" to="/user-profile">
           <v-icon color="tertiary">mdi-account</v-icon>
         </router-link>
       </v-flex>
@@ -91,59 +46,56 @@
 </template>
 
 <script>
-
-import {
-  mapMutations
-} from 'vuex'
+import { mapMutations } from "vuex";
 
 export default {
   data: () => ({
     notifications: [
-      'Mike, John responded to your email',
-      'You have 5 new tasks',
-      'You\'re now a friend with Andrew',
-      'Another Notification',
-      'Another One'
+      "Mike, John responded to your email",
+      "You have 5 new tasks",
+      "You're now a friend with Andrew",
+      "Another Notification",
+      "Another One"
     ],
     title: null,
     responsive: false
   }),
 
   watch: {
-    '$route' (val) {
-      this.title = val.name
+    $route(val) {
+      this.title = val.name;
     }
   },
 
-  mounted () {
-    this.onResponsiveInverted()
-    window.addEventListener('resize', this.onResponsiveInverted)
+  mounted() {
+    this.onResponsiveInverted();
+    window.addEventListener("resize", this.onResponsiveInverted);
   },
-  beforeDestroy () {
-    window.removeEventListener('resize', this.onResponsiveInverted)
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResponsiveInverted);
   },
 
   methods: {
-    ...mapMutations('app', ['setDrawer', 'toggleDrawer']),
-    onClickBtn () {
-      this.setDrawer(!this.$store.state.app.drawer)
+    ...mapMutations("app", ["setDrawer", "toggleDrawer"]),
+    onClickBtn() {
+      this.setDrawer(!this.$store.state.app.drawer);
     },
-    onClick () {
+    onClick() {
       //
     },
-    onResponsiveInverted () {
+    onResponsiveInverted() {
       if (window.innerWidth < 991) {
-        this.responsive = true
+        this.responsive = true;
       } else {
-        this.responsive = false
+        this.responsive = false;
       }
     }
   }
-}
+};
 </script>
 
 <style>
-  #core-toolbar a {
-    text-decoration: none;
-  }
+#core-toolbar a {
+  text-decoration: none;
+}
 </style>
