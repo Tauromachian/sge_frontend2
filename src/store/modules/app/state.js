b32129cbcc6874932dae40dd6b53590dca9a9871
jogarcia <jogarcia@uclv.cu>
2020-02-07 11:56:17 -0500

    Delete image url from app state

--- src/store/modules/app/state.js
+++ src/store/modules/app/state.js
@@ -1,7 +1,6 @@
 export default {
   drawer: null,
   color: 'info',
-  image: 'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-2.32103624.jpg',
   services: [
     'Planta piloto', 'MFC'
   ]
