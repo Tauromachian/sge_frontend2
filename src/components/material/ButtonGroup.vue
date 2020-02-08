eaecf848ce548428e2249139c44b2e1828505f76
jogarcia <jogarcia@uclv.cu>
2020-02-08 13:10:35 -0500

    Create the button group component

---
+++ src/components/material/ButtonGroup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ label }}</p>
+    <v-btn-toggle>
+      <v-btn v-for="(button, index) in buttons" :key="index"> {{ button }}</v-btn>
+    </v-btn-toggle>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ButtonGroup",
+  props: {
+    label: String,
+    buttons: Array
+  }
+};
+</script>
