202828fa4718bc935eeabc5a8ac302fbb028d5de
jogarcia <jogarcia@uclv.cu>
2020-02-08 13:09:08 -0500

    Make the transformers form component

---
+++ src/components/app/TransformerForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-text-field label="Capacidad (kVA)" v-model="serviceName" />
+    <v-flex xs12 md4>
+      <v-select label="Tipo" :items="transformersItems" v-model="transformerType" />
+    </v-flex>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TransformerForm",
+  data: function() {
+    return {
+      transformerType: "",
+      transformersItems: ["Monofasico, Trifasico"]
+    };
+  }
+};
+</script>
