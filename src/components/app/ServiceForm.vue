2dec83ad3ed26321520ba3fefbc018cd389a30a8
jogarcia <jogarcia@uclv.cu>
2020-02-14 09:06:33 -0500

    Make client sugested changes to form

--- src/components/app/ServiceForm.vue
+++ src/components/app/ServiceForm.vue
@@ -10,29 +10,29 @@
               :rules="serviceNameRules"
             />
           </v-flex>
-          <v-flex xs12 md6>
+          <v-flex xs12 md3>
             <v-text-field label="Codigo del servicio" v-model="serviceCode" :rules="serviceCodeRules"/>
-          </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field label="CRF" v-model="crf" :rules="crfRules"/>
           </v-flex>
           <v-flex xs6 md3>
             <v-switch label="Exclusivo" v-model="exclusive" />
           </v-flex>
+          <v-flex xs12 md3>
+            <v-text-field label="CRF" v-model="crf" :rules="crfRules"/>
+          </v-flex>
+          
           <v-flex xs6 md3>
-            <v-subheader class="pl-0">Turnos</v-subheader>
+            <v-subheader class="pl-0">Turnos de trabajo</v-subheader>
             <v-slider v-model="turn" :thumb-size="24" thumb-label="always" :max="3" :min="1"></v-slider>
           </v-flex>
-          <v-flex xs12 md6>
+          <v-flex xs12 md2>
             <v-select label="Tipo de tarifa" v-model="tarifType" :items="tarifItems" />
           </v-flex>
 
-          <v-flex md12 class="pt-0 pb-0"></v-flex>
 
-          <v-flex xs12 md4>
+          <v-flex xs12 md2>
             <v-select label="Alimentacion" v-model="alimentation" :items="alimentationItems" />
           </v-flex>
-          <v-flex xs12 md4>
+          <v-flex xs12 md3>
             <v-text-field label="Demanda Contratada (kW)" v-model="hiredDemand" :rules="hiredDemandRules"/>
           </v-flex>
           <v-flex xs12 md4>
@@ -47,7 +47,7 @@
           <v-flex xs12 md4 v-if="metrage == 'Alta'">
             <v-text-field label="Capacidad" v-model="capacity" :rules="capacityRules"/>
           </v-flex>
-          <v-flex xs12 md2 v-else>
+          <v-flex xs12 md4 v-else>
             <v-subheader class="pl-0">Cantidad de transformadores</v-subheader>
 
             <v-slider
@@ -92,12 +92,12 @@ export default {
       capacity: "",
       alimentationItems: ["Simple", "Doble"],
       metrageItems: ["Baja", "Alta"],
-      tarifItems: ["MA1", "MC1", "B1"],
+      tarifItems: ["M1-A", "M1-C", "B1"],
       transformersAmount: 0,
       transformersItems: [1, 2, 3],
       serviceNameRules: [v => !!v || "El nombre del servicio falta"],
       serviceCodeRules: [v => !!v || "El codigo del servicio falta"],
-      crfRules: [v => !!v || "El codigo del servicio falta"],
+      crfRules: [v => !!v || "El CRF falta"],
       hiredDemandRules: [v => !!v || "La demanda contratada falta"],
       meterNumberRules: [v => !!v || "El numero del metro falta"],
       capacityRules: [v => !!v || "La capacidad falta"],
