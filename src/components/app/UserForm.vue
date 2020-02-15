1b1bcf0550b857a3f9fae2a8c42dd31ee791ca62
jogarcia <jogarcia@uclv.cu>
2020-02-14 20:52:25 -0500

    Make the basic UserForm

---
+++ src/components/app/UserForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <material-card color="primary" title="Formulario de servicio" text="Inserta un nuevo servicio">
+    <v-form ref="form">
+      <v-container py-0>
+        <v-layout wrap>
+          <v-flex xs12 md6>
+            <v-text-field label="Nombre" v-model="serviceName" :rules="nameRules" />
+          </v-flex>
+          <v-flex xs12 md6>
+            <v-text-field label="Email" v-model="serviceCode" :rules="emailRules" />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
+  </material-card>
+</template>
+
+<script>
+export default {
+  name: "UserForm"
+};
+</script>
