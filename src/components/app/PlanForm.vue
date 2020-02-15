4b7ef759ccb8290d501385fb3d8a679537d802d8
jogarcia <jogarcia@uclv.cu>
2020-02-14 20:51:21 -0500

    Modifing the layout of the PlanForm

--- src/components/app/PlanForm.vue
+++ src/components/app/PlanForm.vue
@@ -1,52 +1,51 @@
 <template>
-  <material-card color="primary" title="Formulario de planes" text="Inserta un nuevo plan">
+  <material-card color="primary" title="Plan anual" text="Inserta un nuevo plan">
     <v-form ref="form">
       <v-container py-0>
         <v-layout wrap>
+          
           <v-flex xs12 md6>
-            <v-autocomplete label="Servicio" :items="services" :rules="serviceRules"/>
+            <v-text-field  label="Año" />
           </v-flex>
+
           <v-flex xs12 md6>
-            <v-text-field :rules="yearPlanRules" label="Año" />
+            <v-text-field :rules="yearPlanRules" label="Total para el Año (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Plan del año" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Enero (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Enero" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Febrero (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Febrero" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Marzo (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Marzo" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Abril (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Abril" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Mayo (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Mayo" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Junio (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Junio" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Julio (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Julio" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Agosto (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Agosto" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Septiembre (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Septiembre" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Octubre (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Octubre" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Noviembre (kWh)" />
           </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Noviembre" />
-          </v-flex>
-          <v-flex xs12 md6>
-            <v-text-field :rules="monthPlanRules" label="Diciembre" />
+          <v-flex xs12 md3>
+            <v-text-field :rules="monthPlanRules" label="Diciembre (kWh)" />
           </v-flex>
 
           <v-flex xs12 text-xs-right>
@@ -70,7 +69,7 @@ export default {
       metrageSelect: "Alta",
       metrageItems: ["Baja", "Alta"],
       serviceRules: [v => !!v|| "Entre un servicio"],
-      yearPlanRules: [v => !!v || "Entre el plan de este mes"],
+      yearPlanRules: [v => !!v || "Entre el total para el año"],
       monthPlanRules: [v => !!v || "Entre el plan de este mes"]
     };
   },
