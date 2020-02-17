c53c3893a947a39d175ae64d8ed150cb1902baf5
jogarcia <jogarcia@uclv.cu>
2020-02-17 13:27:39 -0500

    Create IotPlatfrom view

---
+++ src/views/IotPlatform.vue
@@ -0,0 +1,11 @@
+<template>
+  <material-card>
+    <iframe class="iframe" src="https://www.google.com"></iframe>
+  </material-card>
+</template>
+
+<style scoped>
+  .iframe{
+    width: inherit;
+  }
+</style>
