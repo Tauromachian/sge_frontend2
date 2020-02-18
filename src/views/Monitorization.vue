dd1a1d60a30711397451157916f1a0ab70dd4a31
jogarcia <jogarcia@uclv.cu>
2020-02-18 14:11:50 -0500

    Change a name

---
+++ src/views/Monitorization.vue
@@ -0,0 +1,27 @@
+<template>
+  <iframe
+    class="iframe"
+    src="https://thingsboard.uclv.cu/dashboard/d0e72800-310c-11ea-8ed0-9d17cbe9ab55?publicId=9c237170-3173-11ea-8ed0-9d17cbe9ab55"
+    frameborder="0"
+  ></iframe>
+</template>
+
+<script>
+export default {
+  name: "Monitorization"
+}
+</script>
+
+<style scoped>
+html,
+body {
+  overflow: hidden;
+  margin: auto;
+  height: 100%;
+  width: 100%;
+}
+.iframe {
+  width: 100%;
+  height: 485px;
+}
+</style>
