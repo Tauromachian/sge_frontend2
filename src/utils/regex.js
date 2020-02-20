512b1159fb9b2ac1207636e61efce69090323c9c
jogarcia <jogarcia@uclv.cu>
2020-02-20 15:37:05 -0500

    Add isEmail to regex test

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -2,6 +2,7 @@ const numberRegex = /^[0-9]+$/;
 const lettersRegex = /^([a-zA-Z])+$/;
 const lettersBlankSpacesRegex = /^([a-zA-Z ])+$/;
 const numberLettersBlankSpacesRegex = /^([0-9a-zA-Z ])+$/;
+const emailRegex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
 const isNumber = string => {
   return numberRegex.test(string);
@@ -19,9 +20,14 @@ const isNumberAndLettersWithWhiteSpaces = string => {
   return numberLettersBlankSpacesRegex.test(string);
 };
 
+const isEmail = string => {
+  return emailRegex.test(string);
+}
+
 module.exports = {
   isNumber,
   isOnlyLetters,
   isLettersWithBlankSpaces,
-  isNumberAndLettersWithWhiteSpaces
+  isNumberAndLettersWithWhiteSpaces,
+  isEmail
 };
