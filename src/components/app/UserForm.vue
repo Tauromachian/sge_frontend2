0cfc91288ece5c02659ad42154bd7d8186651e31
jogarcia <jogarcia@uclv.cu>
2020-02-20 15:20:52 -0500

    Make the user form

--- src/components/app/UserForm.vue
+++ src/components/app/UserForm.vue
@@ -7,7 +7,29 @@
             <v-text-field label="Nombre" v-model="serviceName" :rules="nameRules" />
           </v-flex>
           <v-flex xs12 md6>
+            <v-text-field label="Apellidos" v-model="serviceName" :rules="nameRules" />
+          </v-flex>
+          <v-flex xs12 md6>
+            <v-text-field label="Telefono" v-model="serviceName" :rules="nameRules" />
+          </v-flex>
+          <v-flex xs12 md6>
             <v-text-field label="Email" v-model="serviceCode" :rules="emailRules" />
+          </v-flex>
+          <v-flex xs12 md6>
+            <v-text-field label="Institucion" v-model="serviceCode" :rules="emailRules" />
+          </v-flex>
+          <v-flex xs12 md6>
+            <v-text-field label="Roles" v-model="serviceCode" :rules="emailRules" />
+          </v-flex>
+          <v-flex xs12 md6>
+            <v-text-field label="Fotos" v-model="serviceCode" :rules="emailRules" />
+          </v-flex>
+          <v-flex xs12 md6>
+            <v-text-field label="Cargo" v-model="serviceCode" :rules="emailRules" />
+          </v-flex>
+
+          <v-flex xs12 text-xs-right>
+            <v-btn class="mx-0 font-weight-light">Agregar</v-btn>
           </v-flex>
         </v-layout>
       </v-container>
