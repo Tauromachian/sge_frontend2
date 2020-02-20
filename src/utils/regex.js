b5a2169d9068ec24d55e525d19816e8344db40e4
jogarcia <jogarcia@uclv.cu>
2020-02-20 15:43:41 -0500

    Err Fix email rules

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -2,7 +2,7 @@ const numberRegex = /^[0-9]+$/;
 const lettersRegex = /^([a-zA-Z])+$/;
 const lettersBlankSpacesRegex = /^([a-zA-Z ])+$/;
 const numberLettersBlankSpacesRegex = /^([0-9a-zA-Z ])+$/;
-const emailRegex = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+const emailRegex = /^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
 
 const isNumber = string => {
   return numberRegex.test(string);
