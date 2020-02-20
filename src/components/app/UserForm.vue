d96ad4e158924e31cf9a4f7de27d4e17f09306ef
jogarcia <jogarcia@uclv.cu>
2020-02-20 15:43:26 -0500

    Make User Form rules

--- src/components/app/UserForm.vue
+++ src/components/app/UserForm.vue
@@ -4,32 +4,32 @@
       <v-container py-0>
         <v-layout wrap>
           <v-flex xs12 md6>
-            <v-text-field label="Nombre" v-model="serviceName" :rules="nameRules" />
+            <v-text-field label="Nombre" v-model="name" :rules="nameRules" />
           </v-flex>
           <v-flex xs12 md6>
-            <v-text-field label="Apellidos" v-model="serviceName" :rules="nameRules" />
+            <v-text-field label="Apellidos" v-model="lastName" :rules="nameRules" />
           </v-flex>
           <v-flex xs12 md6>
-            <v-text-field label="Telefono" v-model="serviceName" :rules="nameRules" />
+            <v-text-field label="Telefono" v-model="phoneNumber" :rules="phoneRules" />
           </v-flex>
           <v-flex xs12 md6>
-            <v-text-field label="Email" v-model="serviceCode" :rules="emailRules" />
+            <v-text-field label="Email" v-model="email" :rules="emailRules" />
           </v-flex>
           <v-flex xs12 md6>
-            <v-text-field label="Institucion" v-model="serviceCode" :rules="emailRules" />
+            <v-text-field label="Institucion" v-model="institution" :rules="institutionRules" />
           </v-flex>
           <v-flex xs12 md6>
-            <v-text-field label="Roles" v-model="serviceCode" :rules="emailRules" />
+            <v-text-field label="Roles" v-model="rol" />
           </v-flex>
           <v-flex xs12 md6>
-            <v-text-field label="Fotos" v-model="serviceCode" :rules="emailRules" />
+            <v-text-field label="Foto" v-model="picture" />
           </v-flex>
           <v-flex xs12 md6>
-            <v-text-field label="Cargo" v-model="serviceCode" :rules="emailRules" />
+            <v-text-field label="Cargo" v-model="position" :rules="positionRules" />
           </v-flex>
 
           <v-flex xs12 text-xs-right>
-            <v-btn class="mx-0 font-weight-light">Agregar</v-btn>
+            <v-btn class="mx-0 font-weight-light" color="primary">Agregar</v-btn>
           </v-flex>
         </v-layout>
       </v-container>
@@ -38,7 +38,42 @@
 </template>
 
 <script>
+import {
+  isLettersWithBlankSpaces,
+  isNumberAndLettersWithWhiteSpaces,
+  isEmail
+} from "@/utils/regex";
+
 export default {
-  name: "UserForm"
+  name: "UserForm",
+  data() {
+    return {
+      name: "",
+      lastName: "",
+      phoneNumber: "",
+      email: "",
+      institution: "",
+      rol: "",
+      picture: "",
+      position: "",
+      nameRules: [
+        v => !!v || "Nombre es requerido",
+        v => isLettersWithBlankSpaces(v) || "Caracteres no permitidos"
+      ],
+      phoneRules: [v => !!v || "El número de telefono es requerido"],
+      emailRules: [
+        v => !!v || "E-mail es requerido",
+        v => isEmail(v) || "E-mail must be valid"
+      ],
+      institutionRules: [
+        v => !!v || "La institucion es requerida",
+        v => isNumberAndLettersWithWhiteSpaces(v) || "Caracteres no permitidos"
+      ],
+      positionRules: [
+        v => !!v || "El cargo es requerido",
+        v => isNumberAndLettersWithWhiteSpaces(v) || "Caracteres no permitidos"
+      ]
+    };
+  }
 };
 </script>
