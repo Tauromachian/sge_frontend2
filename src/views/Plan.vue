70b2fed919b04a00768eb6f8306ec17df6c030a1
jogarcia <jogarcia@uclv.cu>
2020-02-26 12:13:01 -0500

    Put table before form

--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -6,16 +6,16 @@
           <v-tabs color="transparent" slider-color="white" background-color="primary">
             <v-tabs-slider></v-tabs-slider>
             <v-tab class="mr-3">
-              <v-icon class="mr-2">mdi-bug</v-icon>Crear
-            </v-tab>
-            <v-tab class="mr-3">
               <v-icon class="mr-2">mdi-code-tags</v-icon>Administrar
             </v-tab>
-            <v-tab-item>
-              <app-plan-form class="ml-10 mr-10"></app-plan-form>
-            </v-tab-item>
+            <v-tab class="mr-3">
+              <v-icon class="mr-2">mdi-bug</v-icon>Crear
+            </v-tab>
             <v-tab-item>
               <app-plans-table></app-plans-table>
+            </v-tab-item>
+            <v-tab-item>
+              <app-plan-form class="ml-10 mr-10"></app-plan-form>
             </v-tab-item>
           </v-tabs>
         </v-card>
--- src/views/Service.vue
+++ src/views/Service.vue
@@ -10,16 +10,16 @@
           >
             <v-tabs-slider></v-tabs-slider>
             <v-tab class="mr-3">
-              <v-icon class="mr-2">mdi-bug</v-icon>Crear
-            </v-tab>
-            <v-tab class="mr-3">
               <v-icon class="mr-2">mdi-file-table</v-icon>Administrar
             </v-tab>
-            <v-tab-item>
-              <app-service-form class="ml-10 mr-10"></app-service-form>
-            </v-tab-item>
+            <v-tab class="mr-3">
+              <v-icon class="mr-2">mdi-bug</v-icon>Crear
+            </v-tab>
             <v-tab-item>
               <app-service-table></app-service-table>
+            </v-tab-item>
+            <v-tab-item>
+              <app-service-form class="ml-10 mr-10"></app-service-form>
             </v-tab-item>
           </v-tabs>
         </v-card>
